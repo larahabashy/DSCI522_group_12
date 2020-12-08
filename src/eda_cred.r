d54195de0862499b7a3328857ecb24fffab3c65a
larahabashy <laraahabashy@gmail.com>
2020-12-08 13:51:43 -0800

    added plots for class imbalance and beautified existing plots

--- src/eda_cred.r
+++ src/eda_cred.r
@@ -19,7 +19,6 @@ library(docopt)
 library(ggthemes)
 #library(graphics)
 library(ggcorrplot)
-library(arrow)
 
 theme_set(theme_minimal())
 
@@ -36,33 +35,80 @@ main <- function(train, train_scaled, out_dir) {
   }
   
   # read data 
-  training_data <- arrow::read_feather(train)
-  training_scaled <- arrow::read_feather(train_scaled)
+  training_data <- read_feather(train)
+  training_scaled <- read_feather(train_scaled)
   
   #density plot
-  density_plot <- ggplot(training_scaled) +
-    aes(x = limit_bal,
-        fill = default,
-        color = default) +
+  density_plot <- training_scaled %>%
+    ggplot(aes(x = limit_bal,
+               fill = default)) +
     geom_density(alpha = 0.6) +
     xlab("Credit Limit") +
     ylab("Density") +
     ggtitle("Density of Credit Limit")
+  
+  density_plot <- density_plot + guides(fill=guide_legend(title="Default Flag")) 
+  density_plot <- density_plot + scale_shape_discrete(labels = c("Non-Defaults", "Defaults")) + scale_fill_discrete(labels = c("Non-Defaults", "Defaults"))
   
   ggsave(paste0(out_dir, "/density_plot.png"), 
          density_plot,
          width = 8, 
          height = 10)
   
-  training_scaled$education <- as.factor(toupper(names(training_scaled[,25:31])[max.col(training_scaled[,25:31])]))
+  #education count colort by default
+  education_histogram <- training_data %>% 
+    ggplot(aes(x=factor(education), fill=default)) +
+    geom_bar(position="dodge") + 
+    ggtitle("Education levels of Defaulting Clients") +
+    xlab("Education") +
+    ylab("Count")  
   
-  #histogram
-  education_histogram <- ggplot(training_scaled, aes(pay_1, fill = default)) + 
-    geom_histogram(binwidth = 1) + 
-    ggtitle("Histogram of Education Levels") +
-    facet_grid(.~education) + 
-    theme_fivethirtyeight() 
+  education_histogram <- education_histogram + guides(fill=guide_legend(title="Default Flag")) 
+  education_histogram <- education_histogram + scale_shape_discrete(labels = c("Non-Defaults", "Defaults")) + scale_fill_discrete(labels = c("Non-Defaults", "Defaults"))
+  education_histogram <-  education_histogram + scale_x_discrete(labels=c("0" = "Undefined", "1" = "masters",
+                                                                          "2" = "bachelors", "3" = "highschool",
+                                                                          "4" = "school", "5" = "other",
+                                                                          "6" = "other"))
   
+  ggsave(paste0(out_dir, "/education_histogram.png"), 
+         education_histogram,
+         width = 8, 
+         height = 10)
+  
+  count_plot <- training_data %>% 
+    ggplot(aes(x=as.numeric(default), fill=default)) +
+    geom_bar() +
+    geom_text(stat='count', aes(label=..count..), vjust=3, hjust=0.9, color = 'black') + 
+    ggtitle("Count of Defaulting Clients") +
+    xlab("Defaults") +
+    ylab("Count") 
+  
+  count_plot <- prop_plot + guides(fill=guide_legend(title="Default Flag")) 
+  count_plot <- prop_plot + scale_shape_discrete(labels = c("Non-Defaults", "Defaults")) + scale_fill_discrete(labels = c("Non-Defaults", "Defaults"))
+  
+  ggsave(paste0(out_dir, "/counts_plot.png"), 
+         count_plot,
+         width = 8, 
+         height = 10)
+  
+  prop_plot <- new_df %>% 
+    ggplot(aes(x=as.numeric(default_fact),  y = ..prop.., fill = factor(..x..), group = 1)) +
+    geom_bar(stat='count') +
+    geom_text(stat='count', aes(label=..prop..), vjust=3, hjust=0.5, color = 'black') +
+    scale_y_continuous(labels = scales::percent_format()) + 
+    ggtitle("Proportions of Defaulting Clients") +
+    xlab("Defaults") +
+    ylab("Proportion") 
+  
+  prop_plot <- prop_plot + guides(fill=guide_legend(title="Default Flag")) 
+  prop_plot <- prop_plot + scale_shape_discrete(labels = c("Non-Defaults", "Defaults")) + scale_fill_discrete(labels = c("Non-Defaults", "Defaults"))
+  
+  ggsave(paste0(out_dir, "/proportions_plot.png"), 
+         prop_plot,
+         width = 8, 
+         height = 10)
+  
+  training_data %>% group_by(default) %>% summarise(proportion = round(n()/nrow(.),4))
   ggsave(paste0(out_dir, "/education_histogram.png"), 
          education_histogram,
          width = 8, 
